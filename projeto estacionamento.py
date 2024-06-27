7ec9981c477218389d974b344589b8930c92097b
DanielNacimento1205 <danieldacosta120805@gmail.com>
2024-06-27 15:47:41 -0300

    desafio

---
+++ projeto estacionamento.py
@@ -0,0 +1,9 @@
+n = int(input('quantas vagas estão disponíveis? '))
+m = int(input('diga quanto clientes tem: '))
+V_i = []
+for i in range(1,m,2):
+    if 1 <= n and n <= 100000:
+
+    if 1 <= n and m <= 100000:
+        
+    if 1 <= V_i == 1 <= i <= n:
